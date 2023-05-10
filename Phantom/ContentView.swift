de5ab05e01e99bd747cd1dfc833071dcd0164deb
Aayush9029 <aayushpokharel36@gmail.com>
2023-05-10 00:26:55 -0400

    Initial Commit

---
+++ Phantom/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Phantom
+//
+//  Created by Aayush Pokharel on 2023-05-10.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Phantom/PhantomApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhantomApp.swift
+//  Phantom
+//
+//  Created by Aayush Pokharel on 2023-05-10.
+//
+
+import SwiftUI
+
+@main
+struct PhantomApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
